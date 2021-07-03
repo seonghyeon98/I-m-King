0baa4b929acd50bba6424549664674dab92de9da
Sehw <86810453+Sehw@users.noreply.github.com>
2021-07-04 00:23:10 +0900

    21.07.04

--- Assets/Ryu/Scripts/SpawnerManager.cs
+++ Assets/Ryu/Scripts/SpawnerManager.cs
@@ -33,10 +33,19 @@ public class SpawnerManager : MonoBehaviour
 
     private void SpawnEnemy()
     {
-        // 스포너들을 랜덤으로 골라준다.
 
+        // 웨이브가 시작 되면 
+        if (wave)
+        {
 
+        }
+        // enmeySpawners 배열들 중에서 랜덤으로 스폰 위치를 잡아 스폰한다.
+        // 몬스터들을 소환할 때 게임오브젝트 배열에 담는다.
+        // 몬스터들이 다 쥬그믄 다음 웨이브 시작.
+        // 몬스터들이 죽을 때, 게임오브젝트 배열에서 삭제한다.
 
+        //다음 웨이브는 게임오브젝트 배열의 길이가0일때 시작된다.
+        // 다음 웨이브 때는 몬스터가 증가한데연
     }
 
 
@@ -44,7 +53,6 @@ public class SpawnerManager : MonoBehaviour
     private int GetSpawnNum()
     {
         // 웨이브에 따라 생성량 결정
-        //
         return 0;
     }
 
